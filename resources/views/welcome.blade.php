99c85c5b1b8b9bdfbb3ebe843ad69cad1c864696
k-r04 <rajputkhushal04@gmail.com>
2026-02-17 14:45:15 +0530

    Update welcome.blade.php correct title logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sutariya Infotec</title>
+    <title>Sutariya Infotech</title>
      <!-- SVG Favicon -->
-    <link rel="icon" type="image/svg+xml" href="{{ asset('images/logo.svg') }}">
+    <link rel="icon" href="{{ asset('logo.svg') }}" type="image/svg+xml">
+
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
